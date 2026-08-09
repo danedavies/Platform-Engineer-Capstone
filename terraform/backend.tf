1089bf33094a718249c799ffcfe41aaf8437e35a
Corey <coreyldorsey@gmail.com>
2026-08-09 01:18:03 -0400

    Update S3 bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "capstone-bucket"
+    bucket         = "platform-capstone-bucket"
     key            = "capstone/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
