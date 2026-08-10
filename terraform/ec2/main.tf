d6c15f5074640fc74b86563b178b0c0000b5f43a
Corey <coreyldorsey@gmail.com>
2026-08-10 16:21:40 -0400

    Fixed key pair name

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami           = var.ami
   instance_type = "t3.medium"
 
-  key_name = capstonekey.pem
+  key_name = "capstonekey"
 
   subnet_id     = var.app_public_subnet
   tags = { Name = "${var.project_name}-bastion" }
