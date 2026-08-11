4df8cecef48ffd27fd52be9c15dc751c61e767b4
Corey <coreyldorsey@gmail.com>
2026-08-11 15:21:57 -0400

    Add Router security group and fix undeclared SG reference in EC2 module

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -168,6 +168,38 @@ resource "aws_security_group" "grafana_sg" {
 }
 
 ############################################
+# Router Security Group
+############################################
+resource "aws_security_group" "router" {
+  name        = "${var.project_name}-router-sg"
+  description = "Allow SSH and routing traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from bastion"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  # Router needs wide egress so it can forward traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-router-sg"
+    Project = "capstone"
+    Role    = "router_sg"
+  }
+}
+
+
+############################################
 # Observability Stack
 ############################################
 resource "aws_instance" "grafana" {
