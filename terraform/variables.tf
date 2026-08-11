4ee00490e964d8ae12a0513b55023ea68c55ded3
Corey <coreyldorsey@gmail.com>
2026-08-11 17:24:44 -0400

    Fix EC2 module wiring, add root variables, and correct VPC/subnet inputs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,18 @@ variable "project_name" {
   type    = string
   default = "platform-capstone"
 }
+
+variable "ami" { type = string }
+
+variable "app_vpc_id" { type = string }
+variable "obs_vpc_id" { type = string }
+variable "router_vpc_id" { type = string }
+
+variable "app_public_subnet" { type = string }
+variable "app_private_1" { type = string }
+variable "app_private_2" { type = string }
+
+variable "obs_public_subnet" { type = string }
+variable "obs_private_subnet" { type = string }
+
+variable "router_public_subnet" { type = string }
