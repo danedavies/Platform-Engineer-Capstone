fdefa8342ddb36ad05ba99d87a9d5b83dc425c35
ConnerKlingensmith <connerkling22@gmail.com>
2026-08-11 19:20:21 +0000

    added outputs to vpn/outputs.tf

---
+++ terraform/vpn/outputs.tf
@@ -0,0 +1,31 @@
+output "vpn_tunnel1_address" {
+  value = aws_vpn_connection.app.tunnel1_address
+}
+
+output "vpn_tunnel1_cgw_inside_address" {
+  value = aws_vpn_connection.app.tunnel1_cgw_inside_address
+}
+
+output "vpn_tunnel1_vgw_inside_address" {
+  value = aws_vpn_connection.app.tunnel1_vgw_inside_address
+}
+
+output "vpn_tunnel1_bgp_asn" {
+  value = aws_vpn_connection.app.tunnel1_bgp_asn
+}
+
+output "vpn_tunnel2_address" {
+  value = aws_vpn_connection.app.tunnel2_address
+}
+
+output "vpn_tunnel2_cgw_inside_address" {
+  value = aws_vpn_connection.app.tunnel2_cgw_inside_address
+}
+
+output "vpn_tunnel2_vgw_inside_address" {
+  value = aws_vpn_connection.app.tunnel2_vgw_inside_address
+}
+
+output "vpn_tunnel2_bgp_asn" {
+  value = aws_vpn_connection.app.tunnel2_bgp_asn
+}
