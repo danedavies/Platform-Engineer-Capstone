16ce9a33d4d2814479ee410e02973550b9a89a1c
Corey <coreyldorsey@gmail.com>
2026-08-12 17:42:11 -0400

    Remove unused CIDR variables for VPCs from variables.tf

--- terraform/peering/variables.tf
+++ terraform/peering/variables.tf
@@ -13,18 +13,3 @@ variable "router_vpc_id" {
   type        = string
 }
 
-
-variable "app_cidr" {
-  description = "CIDR block for the App VPC"
-  type        = string
-}
-
-variable "obs_cidr" {
-  description = "CIDR block for the Observability VPC"
-  type = string
-}
-
-variable "router_cidr" {
-  description = "CIDR block for the Router VPC"
-  type = string
-}
